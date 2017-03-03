7d05c39203bd84d9b11e7e5d5ee98811f0779350
Julien Sobczak <julien.sobczak@gmail.com>
2017-03-03 22:35:10 +0100

    Add practice page

--- scripts/application.js
+++ scripts/application.js
@@ -10,17 +10,60 @@ var getCard = function(suit, type) {
 };
 
 
-// Reference panel - Show the full matrix
+
+
 var app = new Vue({
-  el: '#reference',
+  el: '#app',
 
   data: {
+
+    matrix: matrix ,    // inherited from a global variable
+
+    // menu
+    section: "home",    // currently displayed section
+
+    // reference
     showPictures: true, // displays the card pictures
     showTexts: true,    // display the card texts
-    matrix: matrix      // inherited from a global variable
+
+    // practice
+    card1Index: -1,
+    card2Index: -1,
+    card3Index: -1,
+    card1: null,
+    card2: null,
+    card3: null,
   },
 
   methods: {
+
+    // Display the given section after hidding the other ones.
+    toggle: function(idSection) {
+      app.section = idSection;
+      if (idSection == 'practice') {
+        app.initPracticeSession();
+      }
+    },
+
+    getType: function(typeName) {
+      var types = app.matrix.metadata.types;
+      for (var i = 0; i < types.length; i++) {
+        if (types[i].name == typeName) {
+          return types[i];
+        }
+      }
+      return undefined;
+    },
+
+    getSuit: function(suitName) {
+      var suits = app.matrix.metadata.suits;
+      for (var i = 0; i < suits.length; i++) {
+        if (suits[i].name == suitName) {
+          return suits[i];
+        }
+      }
+      return undefined;
+    },
 
     // Return a card PAO phrase from its suit and type metadata.
     getPhrase: function(suit, type) {
@@ -31,10 +74,31 @@ var app = new Vue({
       return result;
     },
 
+    // Return the card CSS class name.
+    getClassName: function(card) {
+      return "card-" + card.suit + "-" + card.type;
+    },
+
     // Return the card picture from its suit and type metadata.
     getSmallPicture: function(suit, type) {
       return '/images/pictures/' + type.filename + '-' + suit.filename + '-small.jpg';
-    }
+    },
+
+    initPracticeSession: function() {
+      // Determine three random cards
+      app.card1Index = Math.floor((Math.random() * matrix.cards.length));
+      do {
+        app.card2Index = Math.floor((Math.random() * matrix.cards.length));
+      } while (app.card2Index == app.card1Index);
+      do {
+        app.card3Index = Math.floor((Math.random() * matrix.cards.length));
+      } while (app.card3Index == app.card1Index || app.card3Index == app.card2Index);
+
+      // Assign the cards
+      app.card1 = app.matrix.cards[app.card1Index];
+      app.card2 = app.matrix.cards[app.card2Index];
+      app.card3 = app.matrix.cards[app.card3Index];
+    },
 
   }
 })
